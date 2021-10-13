7715282ae5e9125c132d0e0a16739bf862735fab
mattcusprime <englmt1@gmail.com>
2021-10-13 13:14:32 -0400

    Create sampleCursor.sql

---
+++ sampleCursor.sql
@@ -0,0 +1,18 @@
+DECLARE
+       @cursItmNumber nvarchar(max);
+
+DECLARE C CURSOR FAST_FORWARD --read only, forward only
+       FOR select itmItemNumber from dbo.items where itmID = 92153;
+
+
+OPEN C;
+FETCH NEXT FROM C INTO @cursItmNumber;
+
+WHILE @@FETCH_STATUS = 0
+       BEGIN
+              FETCH NEXT FROM C INTO @cursItmNumber;
+              Print @cursItmNumber;
+       END
+CLOSE C;
+
+DEALLOCATE C;
